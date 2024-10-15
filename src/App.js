db578d35e11b5184e4941185b0e5ada30263ff87
reetreet123 <123092335+reetreet123@users.noreply.github.com>
2024-10-16 00:13:49 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./Page/Dashboard"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
